d962faf858ab0891adcfc56fe10295bcec5fab79
shaziaabdul <114333102+shaziaabdul@users.noreply.github.com>
2024-08-30 00:10:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function Main(){
+    return (
+        <div>
+    <h4>Hello</h4>
+    <div>World</div>
+    </div>
+);
+}
+ReactDOM.render(<Main/>,document.getElementById("root"));
